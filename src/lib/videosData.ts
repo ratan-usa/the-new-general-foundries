c64339c6b4c0db2e1e720a7f89f8d8d02da823c3
Ratan Prajapati <ratanprajapati1242@gmail.com>
2025-12-31 16:45:50 +0530

    thumbanil added

--- src/lib/videosData.ts
+++ src/lib/videosData.ts
@@ -3,7 +3,7 @@ export const allFactoryVideos = [
         "id": 1,
         "name": "Mega Foundries - Heavy Forge",
         "location": "Texas, USA",
-        "thumbnail": "/assets/thumb1.jpg",
+        "thumbnail": "/assets/image1.jpeg",
         "videoUrl": "/video/video1.mp4",
         "isLive": true
     },
@@ -11,7 +11,7 @@ export const allFactoryVideos = [
         "id": 2,
         "name": "Mega Foundries - Precision Casting",
         "location": "Ohio, USA",
-        "thumbnail": "/assets/thumb2.jpg",
+        "thumbnail": "/assets/image2.jpeg",
         "videoUrl": "/video/video2.mp4",
         "isLive": true
     },
@@ -19,7 +19,7 @@ export const allFactoryVideos = [
         "id": 3,
         "name": "Mega Foundries - Steel Valves",
         "location": "Pennsylvania, USA",
-        "thumbnail": "/assets/thumb3.jpg",
+        "thumbnail": "/assets/image3.jpeg",
         "videoUrl": "/video/video3.mp4",
         "isLive": true
     },
@@ -27,7 +27,7 @@ export const allFactoryVideos = [
         "id": 4,
         "name": "Mega Foundries - Iron Pipes",
         "location": "Michigan, USA",
-        "thumbnail": "/assets/thumb4.jpg",
+        "thumbnail": "/assets/image4.jpg",
         "videoUrl": "/video/video4.mp4",
         "isLive": true
     },
@@ -35,7 +35,7 @@ export const allFactoryVideos = [
         "id": 5,
         "name": "Mega Foundries - Auto Parts",
         "location": "Indiana, USA",
-        "thumbnail": "/assets/thumb5.jpg",
+        "thumbnail": "/assets/image5.jpeg",
         "videoUrl": "/video/video5.mp4",
         "isLive": true
     },
@@ -43,7 +43,7 @@ export const allFactoryVideos = [
         "id": 6,
         "name": "Mega Foundries - Aluminum Die",
         "location": "Illinois, USA",
-        "thumbnail": "/assets/thumb6.jpg",
+        "thumbnail": "/assets/image6.jpg",
         "videoUrl": "/video/video6.mp4",
         "isLive": true
     },
@@ -51,7 +51,7 @@ export const allFactoryVideos = [
         "id": 7,
         "name": "Mega Foundries - Aero Components",
         "location": "California, USA",
-        "thumbnail": "/assets/thumb7.jpg",
+        "thumbnail": "/assets/image7.jpg",
         "videoUrl": "/video/video7.mp4",
         "isLive": true
     },
@@ -59,7 +59,7 @@ export const allFactoryVideos = [
         "id": 8,
         "name": "Mega Foundries - Heavy Machinery",
         "location": "Wisconsin, USA",
-        "thumbnail": "/assets/thumb8.jpg",
+        "thumbnail": "/assets/image8.jpg",
         "videoUrl": "/video/video8.mp4",
         "isLive": true
     },
@@ -67,7 +67,7 @@ export const allFactoryVideos = [
         "id": 9,
         "name": "Mega Foundries - Brass Fittings",
         "location": "New York, USA",
-        "thumbnail": "/assets/thumb9.jpg",
+        "thumbnail": "/assets/image9.jpeg",
         "videoUrl": "/video/video9.mp4",
         "isLive": true
     },
@@ -75,7 +75,7 @@ export const allFactoryVideos = [
         "id": 10,
         "name": "Mega Foundries - Structural Steel",
         "location": "Alabama, USA",
-        "thumbnail": "/assets/thumb10.jpg",
+        "thumbnail": "/assets/image10.jpeg",
         "videoUrl": "/video/video10.mp4",
         "isLive": true
     },
@@ -83,7 +83,7 @@ export const allFactoryVideos = [
         "id": 11,
         "name": "Mega Foundries - Custom Molds",
         "location": "Missouri, USA",
-        "thumbnail": "/assets/thumb11.jpg",
+        "thumbnail": "/assets/image11.jpg",
         "videoUrl": "/video/video11.mp4",
         "isLive": true
     },
@@ -91,7 +91,7 @@ export const allFactoryVideos = [
         "id": 12,
         "name": "Mega Foundries - Turbine Blades",
         "location": "Florida, USA",
-        "thumbnail": "/assets/thumb12.jpg",
+        "thumbnail": "/assets/image12.jpg",
         "videoUrl": "/video/video12.mp4",
         "isLive": true
     },
@@ -99,7 +99,7 @@ export const allFactoryVideos = [
         "id": 13,
         "name": "Mega Foundries - Railway Parts",
         "location": "Tennessee, USA",
-        "thumbnail": "/assets/thumb13.jpg",
+        "thumbnail": "/assets/image13.jpg",
         "videoUrl": "/video/video13.mp4",
         "isLive": true
     },
@@ -107,7 +107,7 @@ export const allFactoryVideos = [
         "id": 14,
         "name": "Mega Foundries - Marine Propellers",
         "location": "Washington, USA",
-        "thumbnail": "/assets/thumb14.jpg",
+        "thumbnail": "/assets/image14.jpeg",
         "videoUrl": "/video/video14.mp4",
         "isLive": true
     },
@@ -115,7 +115,7 @@ export const allFactoryVideos = [
         "id": 15,
         "name": "Mega Foundries - Hydraulic Pumps",
         "location": "Minnesota, USA",
-        "thumbnail": "/assets/thumb15.jpg",
+        "thumbnail": "/assets/image15.jpg",
         "videoUrl": "/video/video15.mp4",
         "isLive": true
     },
@@ -123,7 +123,7 @@ export const allFactoryVideos = [
         "id": 16,
         "name": "Mega Foundries - Gear Boxes",
         "location": "North Carolina, USA",
-        "thumbnail": "/assets/thumb16.jpg",
+        "thumbnail": "/assets/image16.jpg",
         "videoUrl": "/video/video16.mp4",
         "isLive": true
     },
@@ -131,7 +131,7 @@ export const allFactoryVideos = [
         "id": 17,
         "name": "Mega Foundries - Manhole Covers",
         "location": "Georgia, USA",
-        "thumbnail": "/assets/thumb17.jpg",
+        "thumbnail": "/assets/image17.jpg",
         "videoUrl": "/video/video17.mp4",
         "isLive": true
     },
@@ -139,7 +139,7 @@ export const allFactoryVideos = [
         "id": 18,
         "name": "Mega Foundries - Pump Casings",
         "location": "South Carolina, USA",
-        "thumbnail": "/assets/thumb18.jpg",
+        "thumbnail": "/assets/image18.jpeg",
         "videoUrl": "/video/video18.mp4",
         "isLive": true
     },
@@ -147,7 +147,7 @@ export const allFactoryVideos = [
         "id": 19,
         "name": "Mega Foundries - Agricultural Tools",
         "location": "Iowa, USA",
-        "thumbnail": "/assets/thumb19.jpg",
+        "thumbnail": "/assets/image19.jpeg",
         "videoUrl": "/video/video19.mp4",
         "isLive": true
     },
@@ -155,7 +155,7 @@ export const allFactoryVideos = [
         "id": 20,
         "name": "Mega Foundries - Medical Implants",
         "location": "Massachusetts, USA",
-        "thumbnail": "/assets/thumb20.jpg",
+        "thumbnail": "/assets/image20.jpg",
         "videoUrl": "/video/video20.mp4",
         "isLive": true
     },
@@ -163,7 +163,7 @@ export const allFactoryVideos = [
         "id": 21,
         "name": "Mega Foundries - Construction Beams",
         "location": "Arizona, USA",
-        "thumbnail": "/assets/thumb21.jpg",
+        "thumbnail": "/assets/image1.jpeg",
         "videoUrl": "/video/video21.mp4",
         "isLive": true
     },
@@ -171,7 +171,7 @@ export const allFactoryVideos = [
         "id": 22,
         "name": "Mega Foundries - Defense Equipment",
         "location": "Virginia, USA",
-        "thumbnail": "/assets/thumb22.jpg",
+        "thumbnail": "/assets/image2.jpeg",
         "videoUrl": "/video/video22.mp4",
         "isLive": true
     },
@@ -179,7 +179,7 @@ export const allFactoryVideos = [
         "id": 23,
         "name": "Mega Foundries - Oil & Gas Valves",
         "location": "Louisiana, USA",
-        "thumbnail": "/assets/thumb23.jpg",
+        "thumbnail": "/assets/image3.jpeg",
         "videoUrl": "/video/video23.mp4",
         "isLive": true
     },
@@ -187,7 +187,7 @@ export const allFactoryVideos = [
         "id": 24,
         "name": "Mega Foundries - Mining Tools",
         "location": "Nevada, USA",
-        "thumbnail": "/assets/thumb24.jpg",
+        "thumbnail": "/assets/image4.jpg",
         "videoUrl": "/video/video24.mp4",
         "isLive": true
     },
@@ -195,7 +195,7 @@ export const allFactoryVideos = [
         "id": 25,
         "name": "Mega Foundries - Renewable Energy",
         "location": "Oregon, USA",
-        "thumbnail": "/assets/thumb25.jpg",
+        "thumbnail": "/assets/image5.jpeg",
         "videoUrl": "/video/video25.mp4",
         "isLive": true
     }
